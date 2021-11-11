170a463690f64f42722213715660ec2b3a983c4e
方洁 <8927667+tomatoohyeah@user.noreply.gitee.com>
2021-11-11 14:44:29 +0800

    搜索组件

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,24 @@
 <template>
-<div>
-  hi
+<div id="app">
+  <search searchtip="请输入一下子"/>
+  <!-- <test /> -->
 </div>
 </template>
 
 <script>
-
+import Search from './components/search/index.vue'
+import test from './components/other/test.vue'
 export default {
   name: 'App',
   components: {
+    Search,
+    test
   }
 }
 </script>
 
-<style>
+<style lang="scss">
+// #app{
+//   // background-color: beige;
+// }
 </style>
